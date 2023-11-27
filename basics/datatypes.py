3ecd3002c424e244428aff25bdc185f29760656c
Aarti1820 <nehakacha20@gmail.com>
2023-11-27 12:41:39 +0000

    Added list and set to datatypes.py file

--- basics/datatypes.py
+++ basics/datatypes.py
@@ -2,7 +2,12 @@ num1 = 10
 name = "John"
 PIValue = 3.14
 is_alive = True
+
+l1 = [10, 20, 30, 40, 50]
+s1 = {10, 20, 30}
 print(num1)
 print(name)
 print(PIValue)
 print(is_alive)
+print(l1)
+print(s1)
